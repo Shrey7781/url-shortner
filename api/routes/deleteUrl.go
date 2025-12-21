48265d7b43149f29c49ba1d369e130c64f9043ef
Shrey7781 <shreeyansh.saxena.dev@gmail.com>
2025-12-21 20:34:36 +0530

    added all routes

--- api/routes/deleteUrl.go
+++ api/routes/deleteUrl.go
@@ -1,1 +1,40 @@
 package routes
+
+import (
+	"net/http"
+
+	"github.com/Shrey7781/url-shortner/api/database"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+)
+
+func DeleteURL(c *gin.Context) {
+	shortID := c.Param("shortID")
+	r := database.CreateClient(0)
+	defer r.Close()
+	val, err := r.Get(database.Ctx, shortID).Result()
+	
+	if err == redis.Nil || val == "" {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "ShortID dosen't exists",
+		})
+		return
+	}
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "problem connecting to redis server",
+		})
+		return
+	}
+
+	err = r.Del(database.Ctx, shortID).Err()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Unable to Delete shortend link",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Shortened URL deleted successfully",
+	})
+}
--- api/routes/editUrl.go
+++ api/routes/editUrl.go
@@ -1,1 +1,44 @@
 package routes
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/Shrey7781/url-shortner/api/database"
+	"github.com/Shrey7781/url-shortner/api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func EditURL(c *gin.Context){
+	shortID:=c.Param("shortID")
+	var body models.Requset
+
+	if err:=c.ShouldBind(&body);err!=nil{
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Cannot Parse JSON",
+		})
+	} 
+
+	r:=database.CreateClient(0)
+	defer r.Close()
+
+	val,err:= r.Get(database.Ctx,shortID).Result()
+	if err!=nil || val==""{
+		c.JSON(http.StatusNotFound, gin.H{
+			"error":"ShortID dosen't exists",
+		})
+		return
+	}
+	err=r.Set(database.Ctx,shortID,body.URL,body.Expiry*3600*time.Second).Err()
+	if err!=nil{
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error":"Unable to update the shortend content",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "The Content has been Updated !!!",
+	})
+	 
+
+}
